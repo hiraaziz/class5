7a60d738437813755cf8cc62e66e9e5c16eb1978
hiraaziz <hiraaziz1998@gmail.com>
2020-06-20 15:02:40 -0400

    Create CountReducer.js

---
+++ src/CountReducer.js
@@ -0,0 +1,12 @@
+const reducer = (state, action) => {
+    switch (action) {
+    case 'Increment':
+      return state + 1
+    case 'Decrement':
+      return state - 1
+    default:
+        throw new Error()
+    }
+}
+
+    export default reducer;
